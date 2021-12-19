6aa132cef4d81dec6e359244f863ac361abee064
= <danishfaisal.wwe@gmail.com>
2021-12-19 14:25:58 +0530

    Report progress with setInterval

--- timers.js
+++ timers.js
@@ -1,17 +1,23 @@
-const waitTime = 3000;
+const waitTime = 5000;
 const waitInterval = 500;
 let currentTime = 0;
 
 const incTime = () => {
     currentTime += waitInterval;
-    console.log(`waiting ${currentTime / 1000} second(s)`);
+    const p = Math.floor((currentTime / waitTime) * 100);
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    // console.log(`waiting ${currentTime / 1000} second(s)`);
+    process.stdout.write(`waiting ... ${p}%`);
 }
 
 console.log(`setting a ${waitTime / 1000} second(s) delay`);
 
 const timerFinished = () => {
     clearInterval(interval);
-    console.log("done");
+    process.stdout.clearLine();
+    process.stdout.cursorTo(0);
+    process.stdout.write("done");
 }
 
 const interval = setInterval(incTime, waitInterval);
