53652fc3dc228dbe2f5eb262eca805ff58669fc8
= <danishfaisal.wwe@gmail.com>
2021-12-19 14:17:13 +0530

    Incorporate setInterval

--- timers.js
+++ timers.js
@@ -1,7 +1,18 @@
 const waitTime = 3000;
+const waitInterval = 500;
+let currentTime = 0;
+
+const incTime = () => {
+    currentTime += waitInterval;
+    console.log(`waiting ${currentTime / 1000} second(s)`);
+}
 
 console.log(`setting a ${waitTime / 1000} second(s) delay`);
 
-const timerFinished = () => console.log("done");
+const timerFinished = () => {
+    clearInterval(interval);
+    console.log("done");
+}
 
+const interval = setInterval(incTime, waitInterval);
 setTimeout(timerFinished, waitTime);
